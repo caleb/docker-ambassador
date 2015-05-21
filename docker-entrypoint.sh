3e4d99938ed95b2a00fe611231189087715c5584
Caleb Land <caleb@land.fm>
2015-05-20 20:58:59 -0400

    fixing the bugs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,20 +13,20 @@ else
   exit 1
 fi
 
+for a in "${@}"; do
+  args+=("${a}")
+done
+
 for c in "${containers[@]}"; do
   args+=("-name")
   args+=("${c}")
 done
 
-for a in "${@}"; do
-  args+=("${a}")
-done
-
 if [ "${1}" = "ambassador" ]; then
   args=("${args[@]:1}")
 
-  echo "Running grand-ambassador \"/usr/bin/grand-ambassador ${args[@]:1}\""
-  exec /usr/bin/grand-ambassador "${args[@]:1}"
+  echo "Running grand-ambassador \"/usr/bin/grand-ambassador ${args[@]}\""
+  exec /usr/bin/grand-ambassador "${args[@]}"
 else
   exec "${@}"
 fi
